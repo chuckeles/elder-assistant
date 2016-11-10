ffe671cba7ac39e2771b181be0a609e8d1f140ae
Martin Chuckeles <martin@jakubik.info>
2016-11-10 08:32:37 +0100

    Add buttons to change global text size

--- js/global.js
+++ js/global.js
@@ -1,2 +1,16 @@
 $('[data-toggle="tooltip"]').tooltip();
 $('[data-toggle="popover"]').popover();
+
+$('#text-resize-popover').on('shown.bs.popover', function () {
+    $('#text-plus').click(function () {
+        var $html = $('html');
+        var fontSize = $html.css('font-size');
+        $html.css('font-size', (parseInt(fontSize) + 2) + 'px');
+    });
+
+    $('#text-minus').click(function () {
+        var $html = $('html');
+        var fontSize = $html.css('font-size');
+        $html.css('font-size', (parseInt(fontSize) - 2) + 'px');
+    });
+});

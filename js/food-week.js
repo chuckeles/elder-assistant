1cc87744d8fabab5df6bf297d530ca0722529b83
Martin Jakubik <martin@jakubik.info>
2016-11-12 22:03:17 +0100

    Write the page for ordering food for a week

---
+++ js/food-week.js
@@ -0,0 +1,89 @@
+var app = new Vue({
+    el: '#food-week-container',
+    data: {
+        selectedDay: 'monday',
+        selected: {
+            monday: {
+                name: 'Pondelok',
+                food: []
+            },
+            tuesday: {
+                name: 'Utorok',
+                food: []
+            },
+            wednesday: {
+                name: 'Streda',
+                food: []
+            },
+            thursday: {
+                name: 'Štvrtok',
+                food: []
+            },
+            friday: {
+                name: 'Piatok',
+                food: []
+            }
+        }
+    },
+    computed: {
+        totalSelected: function () {
+            var self = this;
+            return Object.keys(self.selected).reduce(function (sum, day) {
+                return sum + self.selected[day].food.length;
+            }, 0);
+        },
+        totalPrice: function () {
+            var self = this;
+            return Object.keys(self.selected).reduce(function (sum, day) {
+                return sum + self.selected[day].food.reduce(function (priceSum, food) {
+                        return priceSum + food.price * food.amount;
+                    }, 0);
+            }, 0);
+        },
+        selectedDayName: function () {
+            return this.selected[this.selectedDay].name;
+        }
+    },
+    methods: {
+        selectDay: function (day) {
+            this.selectedDay = day;
+        },
+        addFromMenu: function (i) {
+            switch (i) {
+                case 0:
+                    this.add({
+                        name: 'Slepačia polievka',
+                        price: .8
+                    }, this.selectedDay);
+                    break;
+                case 1:
+                    this.add({
+                        name: 'Kuracie na prírodno s ryžou',
+                        price: 1.5
+                    }, this.selectedDay);
+                    break;
+                case 2:
+                    this.add({
+                        name: 'Palacinky',
+                        price: 1
+                    }, this.selectedDay);
+                    break;
+            }
+        },
+        add: function (food, day) {
+            this.selected[day].food.push({
+                name: food.name,
+                price: food.price,
+                amount: 1
+            });
+        },
+        remove: function (food, day) {
+            this.selected[day].food.splice(this.selected[day].food.indexOf(food), 1);
+        }
+    },
+    filters: {
+        price: function (number) {
+            return Math.round(number * 100) / 100;
+        }
+    }
+});

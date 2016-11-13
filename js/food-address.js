8981a39a1fdfd9052d4a187f065e0a11fba45dac
Martin Jakubik <martin@jakubik.info>
2016-11-13 23:04:40 +0100

    Clear food for day scenario

--- js/food-address.js
+++ js/food-address.js
@@ -1,11 +1,13 @@
-$( document ).ready(function() {
-    $('#saveSelection').click(function () {
-        addressJSON = {
+$(function () {
+    $('#food-address-form').submit(function (e) {
+        e.preventDefault();
+        var addressJSON = {
             address: $('#adresa').val(),
             mesto: $('#mesto').val(),
             psc: $('#psc').val(),
             telephone: $('#tel-cislo').val()
         };
         localStorage.setItem('address', JSON.stringify(addressJSON));
+        window.location.href = $('#submit').attr('href');
     });
 });
